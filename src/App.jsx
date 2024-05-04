0d62dd4970e28929a634529cf47f441200de561c
PetusDelgado <petusdelgado@gmail.com>
2024-05-04 14:30:22 -0500

    primerAvance

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CharacterList from './components/CharacterList'
+import Character from './components/Character'
+
+function App(){
+  return <div>
+    <h1>Rick and Morty</h1>
+    <CharacterList/>
+ 
+  </div>
+}
+export default App
